641811bdfe49343558981d7f16a1a3aac82bd544
Joseaee <joseescalona1505@gmail.com>
2024-07-11 09:00:03 -0400

    comentario de la funcion layout y correciones

--- src/component/footer.php
+++ src/component/footer.php
@@ -1,7 +1,8 @@
     
     </main>
     <script src="public/js/jquery.min.js"></script>
-    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/sweetalert2.all.min.js"></script>
     <?php
         if(isset($params['scripts'])){
             
